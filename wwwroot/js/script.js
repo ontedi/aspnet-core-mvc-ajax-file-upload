8314eaf890aa65ebde7b66aa3cc328dacc89f399
serkan <universityofcu@gmail.com>
2022-10-25 23:10:29 +0300

    Javascript düzeltmeleri yapıldı.

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,20 +1,25 @@
 ﻿var objInputDosya = document.getElementsByClassName('js-input-dosya')[0];
 objInputDosya.addEventListener("change", function () {
+
     let strDosya = objInputDosya.files[0];
     let strDosya_Adi = document.getElementsByClassName("js-input-dosya-adi")[0].value;
     let objDosya_Yukle_Hedef = document.getElementsByClassName("js-dosya-yukle-hedef")[0];
+
     objDosya_Yukle_Hedef.innerHTML = "Yükleniyor..";
+
     let fdVeriler = new FormData();
     fdVeriler.append("strDosya_Adi", strDosya_Adi);
     fdVeriler.append("fuDosya", strDosya);
-    const options = {
+
+    const optAyarlar = {
         method: "POST",
         mode: "no-cors",
         body: fdVeriler,
         contentType: false,
         processData: false
     }
-    fetch('/ajax/dosya-yukle', options)
+
+    fetch('/ajax/dosya-yukle', optAyarlar)
     .then(function(response) {
         return response.text()
     })
