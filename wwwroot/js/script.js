00ec2a923a7ebd40995627c63299b2922535f060
serkan <universityofcu@gmail.com>
2022-10-25 22:44:41 +0300

    javascript düzeltmeleri yapıldı.

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,50 +1,26 @@
-﻿var strAjax_Dosya_Yukle_Url = "/ajax/dosya-yukle";
-
-/* Ajax Dosya Yükleme İşlemi Tetikleyici (BAŞLANGIÇ) */
-fnAjax_Dosya_Yukle_Tetikleyici();
-function fnAjax_Dosya_Yukle_Tetikleyici() {
-    jQuery("body").on("change", ".jq-dosya-yukle-form-input-file", function () {
-
-        jQuery(".jq-dosya-yukle-form-hedef-div").html("Yükleniyor..");
-
-        var strDosya_Adi = jQuery(".jq-dosya-yukle-form-input-dosya-adi").val();
-        var strDosya = jQuery(".jq-dosya-yukle-form-input-file")[0].files[0];
-
-        var fdVeriler = new FormData();
-        fdVeriler.append("strDosya_Adi", strDosya_Adi);
-        fdVeriler.append("fuDosya", strDosya);
-
-        jQuery.ajax
-            ({
-                type: 'POST',
-                url: "/ajax/dosya-yukle",
-                cache: false,
-                data: fdVeriler,
-                contentType: false,
-                processData: false,
-                success: function (strSonuc) {
-                    jQuery(".jq-dosya-yukle-form-hedef-div").html(strSonuc);
-                },
-                error: function (ajaxIstek, strDurum) {
-                    var strHata_Mesaji = "";
-                    if (ajaxIstek.status === 0) {
-                        strHata_Mesaji = "Bağlantı yok. Lütfen internet bağlantınızı kontrol ediniz.";
-                    } else if (ajaxIstek.status == 404) {
-                        strHata_Mesaji = "Sayfa bulunamadı [404].";
-                    } else if (ajaxIstek.status == 500) {
-                        strHata_Mesaji = "İç Sunucu Hatası [500].";
-                    } else if (strDurum === 'parsererror') {
-                        strHata_Mesaji = "JSON isteği ayrıştırılamadı.";
-                    } else if (strDurum === 'timeout') {
-                        strHata_Mesaji = "Zaman aşımı hatası.";
-                    } else if (strDurum === 'abort') {
-                        strHata_Mesaji = "Ajax isteği iptal edildi.";
-                    } else {
-                        strHata_Mesaji = "Bilinmeyen hata. Hata detayı: " + ajaxIstek.responseText;
-                    }
-                    alert("Hata: " + strHata_Mesaji);
-                }
-            });
+﻿var objDosyaYukleFormInputFile = document.getElementById('js-form-dosya-yukle');
+objDosyaYukleFormInputFile.addEventListener("change", function () {
+    let strDosya_Adi = document.getElementsByClassName("js-input-dosya-adi")[0].value;
+    let strDosya = document.getElementsByClassName("js-input-dosya")[0].files[0];
+    let objDosya_Yukle_Hedef = document.getElementsByClassName("js-dosya-yukle-hedef")[0];
+    let fdVeriler = new FormData();
+    fdVeriler.append("strDosya_Adi", strDosya_Adi);
+    fdVeriler.append("fuDosya", strDosya);
+    const options = {
+        method: "POST",
+        mode: "no-cors",
+        body: fdVeriler,
+        contentType: false,
+        processData: false
+    }
+    fetch('/ajax/dosya-yukle', options)
+    .then(function(response) {
+        return response.text()
+    })
+    .then((sonuc) => {
+        objDosya_Yukle_Hedef.innerHTML = sonuc;
+    }).catch(hata => 
+    { 
+        alert("Bir hata oluştu! Hata detayları: " + hata);
     });
-}
-/* Ajax Dosya Yükleme İşlemi Tetikleyici (BİTİŞ) */
+});
